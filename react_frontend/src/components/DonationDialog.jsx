b997de5305bb142d92cff690b275b3159fca4020
Ashish <ashm@stanford.edu>
2025-01-03 15:48:07 -0800

    text

--- react_frontend/src/components/DonationDialog.jsx
+++ react_frontend/src/components/DonationDialog.jsx
@@ -32,11 +32,11 @@ function DonationDialog({ open, onClose }) {
                     </ul>
                 </Typography>
                 <Typography paragraph>
-                    If this tool has been helpful to you, please consider donating.
+                    If this tool has been helpful to you, please consider donating.  
                     <u>Any amount is greatly appreciated</u>, but <strong>a suggested contribution is 1 dollar per participant per study</strong>. 
-                    However, if you are unable to contribute, please don’t worry! Pingbot is and will always be completely free to use.
                 </Typography>
                 <Typography paragraph>
+                    However, if you are unable to contribute, please don’t worry! Pingbot is and will always be completely free to use.
                     {/* <strong>Suggested contributions per study are:</strong>
                     <ul>
                         <li>$20 per study for individuals</li>
