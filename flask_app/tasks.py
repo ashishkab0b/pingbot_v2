9ce0be66066e4cb4640a9608f06a7680971830d7
Ashish <ashm@stanford.edu>
2025-01-03 18:59:18 -0800

    added compute pr complete to tasks.py

--- flask_app/tasks.py
+++ flask_app/tasks.py
@@ -8,7 +8,9 @@ from models import Ping, Enrollment
 from telegram_messenger import TelegramMessenger
 from flask import current_app
 from message_constructor import MessageConstructor
-from crud import get_pings_to_send, get_pings_for_reminder
+from crud import get_pings_to_send, get_pings_for_reminder, get_pings_by_enrollment_id
+from flask import jsonify
+
 
 @celery.task
 def check_and_send_pings():
@@ -50,6 +52,20 @@ def check_and_send_pings():
             else:
                 # Log failure
                 current_app.logger.error(f"Failed to send ping {ping.id} to telegram_id {telegram_id}")
+                
+                # Recompute probability completed
+            try:
+                all_pings = get_pings_by_enrollment_id(db.session, ping.enrollment_id)
+                already_sent_pings = [p for p in all_pings if p.sent_ts is not None]
+                completed_sent_pings = [p for p in already_sent_pings if p.first_clicked_ts is not None]
+                pr_completed = len(completed_sent_pings) / len(already_sent_pings) if len(already_sent_pings) > 0 else 0.0
+                ping.enrollment.pr_completed = pr_completed
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                current_app.logger.error(f"Failed to update enrollment {ping.enrollment_id} with pr_completed.")
+                current_app.logger.exception(e)
+                return jsonify({"error": "Internal server error."}), 500
 
         # Now handle reminders
         check_and_send_reminders(session, telegram_messenger, now)
